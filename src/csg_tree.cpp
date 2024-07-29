23053fef844cf92825223a85bf406da413a4d4be
Dmitri Smirnov <dmixsmi@gmail.com>
2024-07-29 17:07:15 -0400

    feat: convert GDML geometry to CSG

--- src/csg_tree.cpp
+++ src/csg_tree.cpp
@@ -3,6 +3,8 @@
 #include "G4GDMLParser.hh"
 #include "G4VPhysicalVolume.hh"
 
+#include "G4CX/G4CXOpticks.hh"
+
 using namespace std;
 
 
@@ -13,4 +15,9 @@ void from_gdml(string gdmlpath)
   const G4VPhysicalVolume& world = *parser.GetWorldVolume(); 
 
   cout << "from_gdml: " << gdmlpath << ": " << world.GetName() << endl;
+
+  G4CXOpticks* g4cx = G4CXOpticks::SetGeometry(&world);
+  g4cx->saveGeometry("./csgeom");
+
+  delete g4cx;
 }
