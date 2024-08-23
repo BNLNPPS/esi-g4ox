b4f5238f732515c39ff9efe1b45cc35e630df893
Dmitri Smirnov <dmixsmi@gmail.com>
2024-08-23 17:03:04 -0400

    feat: add executable to generate photons emitted by a torch (#8)

---
+++ src/simtox.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <plog/Init.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
+
+#include <curand_kernel.h>
+
+#include "SysRap/NP.hh"
+#include "SysRap/SEvent.hh"
+#include "SysRap/sphoton.h"
+#include "SysRap/srng.h"
+#include "SysRap/storch.h"
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+  using PLogFormat = plog::TxtFormatter;
+  static plog::ColorConsoleAppender<PLogFormat> consoleAppender;
+  plog::init(plog::debug, &consoleAppender);
+
+  unsigned n_torches = 1; // create one torch
+  unsigned n_photons = 100;
+  bool dump = true;
+
+  NP* gs = NP::Make<float>(n_torches, 6, 4);
+
+  storch* torch = reinterpret_cast<storch*>(gs->bytes());
+
+  for(unsigned torch_id = 0; torch_id < gs->shape[0]; torch_id++)
+  {
+    cout << "torch_id: " << torch_id << endl;
+    // Set all torch parameters
+    storch::FillGenstep(torch[torch_id], torch_id, n_photons, dump);
+  }
+
+  gs->dump();
+  cout << torch->desc() << endl;
+
+  NP* se = NP::Make<int>(n_photons);
+  NP* ph = NP::Make<float>(n_photons, 4, 4);
+
+  const quad6* qtorch = reinterpret_cast<quad6*>(gs->bytes());
+  sphoton* photons = reinterpret_cast<sphoton*>(ph->bytes());
+  int unused = -1;
+
+  srng rng(12345);
+
+  for(unsigned photon_id = 0; photon_id < n_photons; photon_id++)
+  {
+    storch::generate(photons[photon_id], rng, *qtorch, unused, unused);
+  }
+
+  ph->set_meta({"my photon meta info 1", "additional meta info"});
+  ph->dump();
+  ph->save("out/photons.npy");
+
+  return EXIT_SUCCESS;
+}
