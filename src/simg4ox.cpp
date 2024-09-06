31391f73af0d6613b9ab02042f2be2a09236f797
Dmitri Smirnov <dmixsmi@gmail.com>
2024-09-06 09:18:58 -0400

    update simg4ox

--- src/simg4ox.cpp
+++ src/simg4ox.cpp
@@ -1,44 +1,91 @@
 #include <algorithm>
-#include <string>
+#include <filesystem>
 #include <iostream>
+#include <string>
 
+#include <argparse/argparse.hpp>
+
+#include "G4Event.hh"
 #include "G4GDMLParser.hh"
+#include "G4GenericPhysicsList.hh"
 #include "G4RunManager.hh"
 #include "G4VPhysicalVolume.hh"
 #include "G4VUserDetectorConstruction.hh"
-#include "G4GenericPhysicsList.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
 
 #include "G4CX/G4CXOpticks.hh"
 
 using namespace std;
 
 
-struct G4App : public G4VUserDetectorConstruction
+struct DetectorConstruction : G4VUserDetectorConstruction
 {
-  G4VPhysicalVolume* Construct();
-  virtual ~G4App() {}
+  DetectorConstruction(filesystem::path gdml_file) : gdml_file_(gdml_file) {}
+
+  G4VPhysicalVolume* Construct()
+  {
+    G4GDMLParser parser;
+    parser.Read(gdml_file_.string(), false);
+
+    return parser.GetWorldVolume();
+  }
+
+ private:
+
+  filesystem::path gdml_file_;
+};
+
+
+struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
+{
+  void GeneratePrimaries(G4Event* evt) {}
+};
+
+
+struct G4App
+{
+  G4App(filesystem::path gdml_file) :
+    det_cons_(new DetectorConstruction(gdml_file)),
+    prim_gen_(new PrimaryGenerator)
+  {
+  }
+
+  G4VUserDetectorConstruction*   det_cons_;
+  G4VUserPrimaryGeneratorAction* prim_gen_;
 };
 
 
 int main(int argc, char **argv)
 {
-  G4App* g4app = new G4App;
+  argparse::ArgumentParser program("simg4ox", "0.0.0");
 
-  G4RunManager runMgr;
-  runMgr.SetUserInitialization(new G4GenericPhysicsList);
-  runMgr.SetUserInitialization(static_cast<G4VUserDetectorConstruction*>(g4app));
-  runMgr.Initialize();
-  //runMgr.BeamOn(2);
+  string gdml_file;
+
+  program.add_argument("-g", "--gdml")
+    .help("path to GDML file")
+    .default_value(string("geom.gdml"))
+    .nargs(1)
+    .store_into(gdml_file);
+
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const exception& err) {
+    cerr << err.what() << endl;
+    cerr << program;
+    exit(EXIT_FAILURE);
+  }
+
+  // Configure Geant4
+  G4RunManager run_mgr;
+  run_mgr.SetUserInitialization(new G4GenericPhysicsList);
+
+  G4App* g4app = new G4App(gdml_file);
+
+  run_mgr.SetUserInitialization(g4app->det_cons_);
+  run_mgr.SetUserAction(g4app->prim_gen_);
+  run_mgr.Initialize();
+  run_mgr.BeamOn(2);
 
   return EXIT_SUCCESS;
-}
-
-
-G4VPhysicalVolume* G4App::Construct()
-{
-  G4GDMLParser parser;
-  string gdmlpath("geom.gdml");
-  parser.Read(gdmlpath, false);
-
-  return parser.GetWorldVolume();
 }
