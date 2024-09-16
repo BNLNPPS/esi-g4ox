d35b9c77349d7817def284800ba800e6efdcbd23
Dmitri Smirnov <dmixsmi@gmail.com>
2024-09-16 11:44:51 -0400

    simg4ox: define tracking and stepping actions

--- src/simg4ox.cpp
+++ src/simg4ox.cpp
@@ -10,6 +10,7 @@
 #include "G4RunManager.hh"
 #include "G4VModularPhysicsList.hh"
 #include "G4VPhysicalVolume.hh"
+#include "G4VProcess.hh"
 #include "G4VUserDetectorConstruction.hh"
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "G4OpticalPhoton.hh"
@@ -20,6 +21,7 @@
 #include "G4SystemOfUnits.hh"
 #include "G4ThreeVector.hh"
 #include "G4UserSteppingAction.hh"
+#include "G4UserTrackingAction.hh"
 
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
@@ -27,7 +29,12 @@
 
 #include "G4CX/G4CXOpticks.hh"
 #include "SysRap/NP.hh"
+#include "SysRap/SEvt.hh"
+#include "SysRap/STrackInfo.h"
+#include "SysRap/spho.h"
 #include "SysRap/sphoton.h"
+#include "U4/U4StepPoint.hh"
+#include "U4/U4Track.h"
 
 using namespace std;
 
@@ -90,11 +97,104 @@ struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
 };
 
 
-struct SteppingAction : G4UserSteppingAction
+struct SteppingAction : G4UserSteppingAction {
+
+  void UserSteppingAction(const G4Step* step) {
+    static int photon_id = 0;
+
+    if (step->GetTrack()->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()) {
+      cout << "optical photon: " << photon_id << " - "
+           << step->GetPreStepPoint()->GetPhysicalVolume()->GetName() << " - ";
+
+      const G4VProcess* process = step->GetPreStepPoint()->GetProcessDefinedStep();
+
+      if (process == nullptr) {
+        cout << "no process defined" << endl;
+        return;
+      } else {
+        cout << process->GetProcessName() << endl;
+      }
+    } else {
+      return;
+    }
+
+    const G4StepPoint* pre = step->GetPreStepPoint();
+    //const G4StepPoint* post = step->GetPostStepPoint();
+    const G4Track*     track = step->GetTrack();
+    //G4VPhysicalVolume* pv = track->GetVolume();
+
+    SEvt* sev = SEvt::Create_ECPU();
+
+    // populate current photon with pos, mom, pol, time, wavelength
+    sphoton& photon = sev->current_ctx.p;
+
+    const G4ThreeVector& pos = pre->GetPosition();
+    const G4ThreeVector& mom = pre->GetMomentumDirection();
+    const G4ThreeVector& pol = pre->GetPolarization();
+
+    G4double time = pre->GetGlobalTime();
+    G4double energy = pre->GetKineticEnergy();
+    G4double wavelength = h_Planck*c_light/energy ;
+
+    photon.pos.x = pos.x();
+    photon.pos.y = pos.y();
+    photon.pos.z = pos.z();
+    photon.time  = time/ns ;
+
+    photon.mom.x = mom.x();
+    photon.mom.y = mom.y();
+    photon.mom.z = mom.z();
+
+    photon.pol.x = pol.x();
+    photon.pol.y = pol.y();
+    photon.pol.z = pol.z();
+    photon.wavelength = wavelength/nm ;
+
+    cout << photon << endl;
+
+    //spho ulabel{};
+
+    spho* label = STrackInfo<spho>::GetRef(track);
+
+    //STrackInfo<T>* trackinfo = GetTrackInfo(track);
+    cout << "label: " << *label << endl;
+
+    //std::array<int, spho::N> a_label;
+    //label->serialize(a_label);
+    //ulabel.load(a_label);
+
+    //cout << "ulabel: " << ulabel << endl;
+
+    //cout << STrackInfo<spho>::Desc(track) << endl;
+    //STrackInfo<T>* trackinfo = GetTrackInfo(track);
+
+    //G4VUserTrackInformation* ui = track->GetUserInformation();
+
+    //assert( ui && "ui created" );
+
+    //STrackInfo<G4Track>* trackinfo = ui ? static_cast<STrackInfo<G4Track>*>(ui) : nullptr;
+
+    //assert( trackinfo && "trackinfo created" );
+
+    //return trackinfo ? &(trackinfo->label) : nullptr ;
+
+    //assert( label && "label created" );
+    //assert( label && label->isDefined() && "all photons are expected to be labelled" );
+    //cout << "label: " << label << endl;
+    //sev->pointPhoton(ulabel);        // sctx::point copying current into buffers
+    //sev->pointPhoton(*label);        // sctx::point copying current into buffers
+    //cout << label.gs == other.gs && ix == other.ix && id == other.id ;
+  }
+};
+
+
+struct TrackingAction : G4UserTrackingAction
 {
-  void UserSteppingAction(const G4Step* step)
-  {
-    cout << step->GetPreStepPoint()->GetPhysicalVolume()->GetName() << endl;
+  void PostUserTrackingAction(const G4Track*) override {}
+
+  void PreUserTrackingAction(const G4Track* track) override {
+    U4Track::SetFabricatedLabel<spho>(track);
+    sev->beginPhoton(ulabel);
   }
 };
 
@@ -104,13 +204,15 @@ struct G4App
   G4App(filesystem::path gdml_file) :
     det_cons_(new DetectorConstruction(gdml_file)),
     prim_gen_(new PrimaryGenerator),
-    stepping_(new SteppingAction)
+    stepping_(new SteppingAction),
+    tracking_(new TrackingAction)
   {
   }
 
   G4VUserDetectorConstruction*   det_cons_;
   G4VUserPrimaryGeneratorAction* prim_gen_;
   SteppingAction* stepping_;
+  TrackingAction* tracking_;
 };
 
 
@@ -158,6 +260,8 @@ int main(int argc, char **argv)
   G4App* g4app = new G4App(gdml_file);
   run_mgr.SetUserInitialization(g4app->det_cons_);
   run_mgr.SetUserAction(g4app->prim_gen_);
+  run_mgr.SetUserAction(g4app->tracking_);
+  run_mgr.SetUserAction(g4app->stepping_);
   run_mgr.Initialize();
 
   G4UIExecutive *uix = nullptr;
