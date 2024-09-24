1b41e4b0cc5dc56b8a277600acc949e749ff08a1
Dmitri Smirnov <dmixsmi@gmail.com>
2024-09-24 09:56:45 -0400

    add missing G4 user actions (#20)
    Co-authored-by: Gabor Galgoczi <gaborgalgoczi@gmail.com>

--- src/simg4ox.cpp
+++ src/simg4ox.cpp
@@ -7,18 +7,25 @@
 #include "FTFP_BERT.hh"
 #include "G4Event.hh"
 #include "G4GDMLParser.hh"
-#include "G4RunManager.hh"
-#include "G4VModularPhysicsList.hh"
-#include "G4VPhysicalVolume.hh"
-#include "G4VUserDetectorConstruction.hh"
-#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4OpBoundaryProcess.hh"
 #include "G4OpticalPhoton.hh"
 #include "G4OpticalPhysics.hh"
 #include "G4PrimaryVertex.hh"
 #include "G4PrimaryParticle.hh"
 #include "G4PhysicalConstants.hh"
+#include "G4RunManager.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4ThreeVector.hh"
+#include "G4Track.hh"
+#include "G4TrackStatus.hh"
+#include "G4UserEventAction.hh"
+#include "G4UserSteppingAction.hh"
+#include "G4UserTrackingAction.hh"
+#include "G4VModularPhysicsList.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4VProcess.hh"
+#include "G4VUserDetectorConstruction.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
 
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
@@ -26,21 +33,33 @@
 
 #include "G4CX/G4CXOpticks.hh"
 #include "SysRap/NP.hh"
+#include "SysRap/SEvt.hh"
+#include "SysRap/STrackInfo.h"
+#include "SysRap/spho.h"
 #include "SysRap/sphoton.h"
+#include "U4/U4Random.hh"
+#include "U4/U4StepPoint.hh"
+#include "U4/U4Touchable.h"
+#include "U4/U4Track.h"
 
 using namespace std;
 
 
-struct DetectorConstruction : G4VUserDetectorConstruction
-{
+SEvt* sev = nullptr;
+
+struct DetectorConstruction : G4VUserDetectorConstruction {
+
   DetectorConstruction(filesystem::path gdml_file) : gdml_file_(gdml_file) {}
 
-  G4VPhysicalVolume* Construct()
-  {
+  G4VPhysicalVolume* Construct() override {
     G4GDMLParser parser;
     parser.Read(gdml_file_.string(), false);
 
-    return parser.GetWorldVolume();
+    G4VPhysicalVolume* world = parser.GetWorldVolume();
+
+    G4CXOpticks::SetGeometry(world);
+
+    return world;
   }
 
  private:
@@ -49,23 +68,18 @@ struct DetectorConstruction : G4VUserDetectorConstruction
 };
 
 
-struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
-{
-  void GeneratePrimaries(G4Event* event) {
+struct PrimaryGenerator : G4VUserPrimaryGeneratorAction {
 
+  void GeneratePrimaries(G4Event* event) override {
     NP* photons = NP::Make<float>(0, 4, 4);
 
-    cout << "read photons" << endl;
     photons->load("out/photons.npy");
-    //photons->dump();
 
     size_t n_photons = photons->num_items();
     sphoton* sphotons = reinterpret_cast<sphoton*>(photons->bytes());
 
-    for (int i=0; i < n_photons; i++)
-    {
+    for (int i=0; i < n_photons; i++) {
       sphoton &p = sphotons[i];
-      //cout << "val: " << i << ": " << p;
 
       G4ThreeVector position_mm(p.pos.x, p.pos.y, p.pos.z);
       G4double time_ns = p.time;
@@ -85,6 +99,157 @@ struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
       vertex->SetPrimary(particle);
       event->AddPrimaryVertex(vertex);
     }
+
+    sev->SetInputPhoton(photons);
+  }
+};
+
+
+struct EventAction : G4UserEventAction {
+
+  void BeginOfEventAction(const G4Event *event) override {
+    sev->beginOfEvent(event->GetEventID());
+  }
+
+  void EndOfEventAction(const G4Event *event) override {
+    int eventID = event->GetEventID();
+    sev->addEventConfigArray();
+    sev->gather();
+    sev->endOfEvent(eventID);
+
+    // GPU-based simulation
+    G4CXOpticks* gx = G4CXOpticks::Get() ;
+    gx->simulate(eventID, true ) ;
+  }
+};
+
+
+void get_label(spho& ulabel, const G4Track* track)
+{
+  spho* label = STrackInfo<spho>::GetRef(track);
+  assert(label && label->isDefined() && "all photons are expected to be labelled");
+
+  std::array<int, spho::N> a_label;
+  label->serialize(a_label);
+
+  ulabel.load(a_label);
+}
+
+
+struct SteppingAction : G4UserSteppingAction {
+
+  void UserSteppingAction(const G4Step* step) {
+
+    if (step->GetTrack()->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition())
+      return;
+
+    const G4VProcess* process = step->GetPreStepPoint()->GetProcessDefinedStep();
+
+    if (process == nullptr)
+      return;
+
+    const G4Track* track = step->GetTrack();
+    G4VPhysicalVolume* pv = track->GetVolume();
+    const G4VTouchable* touch = track->GetTouchable();
+
+    spho ulabel = {};
+    get_label(ulabel, track);
+
+    const G4StepPoint* pre = step->GetPreStepPoint();
+    const G4StepPoint* post = step->GetPostStepPoint();
+
+    G4ThreeVector delta = step->GetDeltaPosition();
+    double step_mm = delta.mag()/mm  ;
+
+    sev->checkPhotonLineage(ulabel);
+
+    sphoton& current_photon = sev->current_ctx.p ;
+
+    if (current_photon.flagmask_count() == 1) {
+      U4StepPoint::Update(current_photon, pre);  // populate current_photon with pos, mom, pol, time, wavelength
+      sev->pointPhoton(ulabel);  // copying current into buffers
+    }
+
+    bool tir;
+    unsigned flag = U4StepPoint::Flag<G4OpBoundaryProcess>(post, true, tir);
+    bool is_detect_flag = OpticksPhoton::IsSurfaceDetectFlag(flag);
+
+    current_photon.iindex = is_detect_flag ? U4Touchable::ImmediateReplicaNumber(touch) : U4Touchable::AncestorReplicaNumber(touch);
+
+    U4StepPoint::Update(current_photon, post);
+
+    current_photon.set_flag(flag);
+
+    sev->pointPhoton(ulabel);
+  }
+};
+
+
+struct TrackingAction : G4UserTrackingAction
+{
+  const G4Track* transient_fSuspend_track = nullptr;
+
+  void PreUserTrackingAction_Optical_FabricateLabel(const G4Track* track)
+  {
+    U4Track::SetFabricatedLabel<spho>(track);
+    spho* label = STrackInfo<spho>::GetRef(track);
+    assert(label);
+  }
+
+
+  void PreUserTrackingAction(const G4Track* track) override {
+    spho* label = STrackInfo<spho>::GetRef(track);
+
+    if (label == nullptr) {
+      PreUserTrackingAction_Optical_FabricateLabel(track);
+      label = STrackInfo<spho>::GetRef(track);
+    }
+
+    assert(label && label->isDefined());
+
+    std::array<int, spho::N> a_label;
+    label->serialize(a_label);
+
+    spho ulabel = {};
+    ulabel.load(a_label);
+
+    U4Random::SetSequenceIndex(ulabel.id);
+
+    bool resume_fSuspend = track == transient_fSuspend_track ;
+
+    if (ulabel.gen() == 0) {
+      if (resume_fSuspend == false)
+        sev->beginPhoton(ulabel);
+      else
+        sev->resumePhoton(ulabel);
+    } else if ( ulabel.gen() > 0 ) {
+      if (resume_fSuspend == false)
+        sev->rjoinPhoton(ulabel);
+      else
+        sev->rjoin_resumePhoton(ulabel);
+    }
+  }
+
+
+  void PostUserTrackingAction(const G4Track* track) override {
+    G4TrackStatus tstat = track->GetTrackStatus();
+
+    bool is_fStopAndKill = tstat == fStopAndKill;
+    bool is_fSuspend     = tstat == fSuspend;
+    bool is_fStopAndKill_or_fSuspend = is_fStopAndKill || is_fSuspend;
+
+    assert(is_fStopAndKill_or_fSuspend);
+
+    spho ulabel = {};
+    get_label(ulabel, track);
+
+    if (is_fStopAndKill) {
+      U4Random::SetSequenceIndex(-1);
+      sev->finalPhoton(ulabel);
+      transient_fSuspend_track = nullptr;
+    } else if (is_fSuspend) {
+      transient_fSuspend_track = track;
+    }
   }
 };
 
@@ -93,12 +258,18 @@ struct G4App
 {
   G4App(filesystem::path gdml_file) :
     det_cons_(new DetectorConstruction(gdml_file)),
-    prim_gen_(new PrimaryGenerator)
+    prim_gen_(new PrimaryGenerator),
+    stepping_(new SteppingAction),
+    tracking_(new TrackingAction),
+    event_act_(new EventAction)
   {
   }
 
   G4VUserDetectorConstruction*   det_cons_;
   G4VUserPrimaryGeneratorAction* prim_gen_;
+  SteppingAction* stepping_;
+  TrackingAction* tracking_;
+  EventAction*    event_act_;
 };
 
 
@@ -135,6 +306,9 @@ int main(int argc, char **argv)
     exit(EXIT_FAILURE);
   }
 
+  // Create global event
+  sev = SEvt::HighLevelCreate(SEvt::ECPU);
+
   // Configure Geant4
   // The physics list must be instantiated before other user actions
   G4VModularPhysicsList *physics = new FTFP_BERT;
@@ -146,6 +320,9 @@ int main(int argc, char **argv)
   G4App* g4app = new G4App(gdml_file);
   run_mgr.SetUserInitialization(g4app->det_cons_);
   run_mgr.SetUserAction(g4app->prim_gen_);
+  run_mgr.SetUserAction(g4app->event_act_);
+  run_mgr.SetUserAction(g4app->tracking_);
+  run_mgr.SetUserAction(g4app->stepping_);
   run_mgr.Initialize();
 
   G4UIExecutive *uix = nullptr;
