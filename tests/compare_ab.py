f404849d84a2736358e280e91adc9552cc02c1a2
Dmitri Smirnov <dmixsmi@gmail.com>
2024-10-01 19:36:16 -0400

    fix

--- tests/compare_ab.py
+++ tests/compare_ab.py
@@ -4,9 +4,7 @@ import opticks.sysrap.sevt as s
 a = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0/A000/")
 b = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0/B000/")
 
-#np.allclose(a.f.record[:,1:], b.f.record[:,0:-1], rtol=0, atol=1e-5)
-
-assert len(a.f.record) == len(b.f.record)
+assert a.f.record.shape == b.f.record.shape
 
 diff = [i for i, (a, b) in enumerate(zip(a.f.record[:, 1:], b.f.record[:, 0:-1])) if not np.allclose(a, b, rtol=0, atol=1e-5)]
 print(diff)
