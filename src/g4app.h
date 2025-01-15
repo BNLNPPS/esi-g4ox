1911446802aa4b38b14e2ded199665b974ef43c6
Dmitri Smirnov <dmixsmi@gmail.com>
2025-01-14 21:43:39 -0500

    Update src/g4app.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/g4app.h
+++ src/g4app.h
@@ -61,7 +61,7 @@ bool IsSubtractionSolid(G4VSolid *solid)
 
 std::string str_tolower(std::string s)
 {
-    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c){ return std::tolower(c); });
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
     return s;
 }
 
