b2664ad1318174fdca82ef5d61b75ee052e4d7e1
Gabor Galgoczi <gaborgalgoczi@gmail.com>
2025-02-25 11:33:17 -0500

    Removed extra code from OpticksGPU

--- src/Opticks_GPU.cc
+++ src/Opticks_GPU.cc
@@ -26,33 +26,20 @@ int main(int argc, char **argv)
     // SEventConfig::SetDebugLite();
 
     SEvt *evt = SEvt::Create(SEvt::EGPU);
-
-    // Build the full path to the gensteps file
-
     const char *gdmlpath = "/esi/esi-g4ox/geom/pfrich_min_added_parameters.gdml";
     G4GDMLParser parser_;
-
     parser_.Read(gdmlpath, false);
     G4VPhysicalVolume *world = parser_.GetWorldVolume();
     assert(world);
-
     SSim *sim = SSim::CreateOrReuse();
     assert(sim && "sim instance should have been grabbed/created in ctor");
-
     stree *st = sim->get_tree();
     st->get_num_triangulated();
     const U4Tree *tr;
     static U4SensorIdentifier *SensorIdentifier;
     tr = U4Tree::Create(st, world, SensorIdentifier);
     sim->initSceneFromTree(); // not so easy to do at lower level as do not want to change to SSim arg to U4Tree::Create
-                              // for headeronly testing
     CSGFoundry *fd_ = CSGFoundry::CreateFromSim(); // adopts SSim::INSTANCE
-    float4 ce = make_float4(0.f, 0.f, 0.f, 100.f); // TODO: this should come from the geometry
-
-    SEventConfig::SetMaxExtent(
-        ce.w); // must do this config before QEvent::init which happens with CSGOptiX instanciation
-    SEventConfig::SetMaxTime(10.f);
-
     CSGOptiX *cx = CSGOptiX::Create(fd_); // encumbent SSim used for QSim setup in here
     QSim *qs = cx->sim;
     // if(!SEvt::HasInputPhoton(SEvt::EGPU)) SEvt::AddTorchGenstep();
@@ -65,10 +52,8 @@ int main(int argc, char **argv)
     auto endtime = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = endtime - start;
     std::cout << "Simulation time: " << elapsed.count() << " seconds" << std::endl;
-
     SEvt *sev = SEvt::Get_EGPU();
     unsigned int num_hits2 = sev->GetNumHit(0);
     std::cout << "Opticks: NumHits:  " << num_hits2 << std::endl;
-
     return 0;
 }
