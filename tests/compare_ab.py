17785c992a97749119c52818632f4fe3aef975b6
Dmitri Smirnov <dmixsmi@gmail.com>
2025-03-11 09:52:05 -0400

    ci: use esi-shell@1.4.4-devel (#116)

--- tests/compare_ab.py
+++ tests/compare_ab.py
@@ -1,13 +1,12 @@
 import numpy as np
 import opticks.sysrap.sevt as s
 
-a = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0/A000/")
-b = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0/B000/")
+a = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0_none/A000/")
+b = s.SEvt.Load("/tmp/GEOM/fakegeom/simg4ox/ALL0_none/B000/f000")
 
 assert a.f.record.shape == b.f.record.shape
 
 diff = [i for i, (a, b) in enumerate(zip(a.f.record[:, 1:], b.f.record[:, 0:-1])) if not np.allclose(a, b, rtol=0, atol=1e-5)]
 print(diff)
 
-assert diff == [14, 21, 22, 36, 40, 54, 64, 81]
-
+assert diff == [14, 22, 32, 34, 40, 81, 85]
