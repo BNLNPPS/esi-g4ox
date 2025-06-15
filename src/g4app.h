bf1d3390600d980c13851c1bdb04098b1e0e94d3
Gabor Galgoczi <gaborgalgoczi@gmail.com>
2025-06-15 13:07:18 -0400

    Moved gun position to match GenSteps branch

--- src/g4app.h
+++ src/g4app.h
@@ -283,7 +283,7 @@ struct PrimaryGenerator : G4VUserPrimaryGeneratorAction
 
     void GeneratePrimaries(G4Event *event) override
     {
-        G4ThreeVector position_mm(-0.4 * m, -0.3 * m, -0.2 * m);
+        G4ThreeVector position_mm(-0.4 * m, -0.3 * m, -0.3 * m);
         G4double time_ns = 0;
         G4ThreeVector direction(0, 0.2, 0.8);
         G4double wavelength_nm = 0.1;
