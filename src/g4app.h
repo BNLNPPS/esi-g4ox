8eee7f0f3b7b31c36b6d5e977b9b83344a1b20d5
Gabor Galgoczi <gaborgalgoczi@gmail.com>
2025-06-15 13:05:43 -0400

    Killing all particles except optical photons insude mirrpyramid

--- src/g4app.h
+++ src/g4app.h
@@ -400,9 +400,22 @@ struct SteppingAction : G4UserSteppingAction
 
     void UserSteppingAction(const G4Step *aStep)
     {
-        const G4Track *aTrack;
+        G4Track *aTrack;
         G4int fNumPhotons = 0;
-        G4SteppingManager *fpSteppingManager =
+        
+
+        G4StepPoint *preStep = aStep->GetPostStepPoint();
+
+        G4VPhysicalVolume *volume = preStep->GetPhysicalVolume();	
+	if (volume && volume->GetName() == "MirrorPyramid")
+        {
+		aTrack = aStep->GetTrack();
+            if (aTrack->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition())
+            {
+                aTrack->SetTrackStatus(fStopAndKill);
+            }}
+	
+	G4SteppingManager *fpSteppingManager =
             G4EventManager::GetEventManager()->GetTrackingManager()->GetSteppingManager();
         G4StepStatus stepStatus = fpSteppingManager->GetfStepStatus();
         if (stepStatus != fAtRestDoItProc)
@@ -530,3 +543,4 @@ struct G4App
     SteppingAction *stepping_;
     TrackingAction *tracking_;
 };
+
