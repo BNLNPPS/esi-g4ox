6994339cb423c956e301e05f6a317a357018afe5
ggalgoczi <gaborgalgoczi@gmail.com>
2025-07-26 16:56:33 -0400

    Updating filenames

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd ..
 cmake -S esi-g4ox -B build
 cmake --build build
 QCurandState_SPEC=3000000:0:0 /usr/local/opticks/lib/QCurandStateTest
-./build/src/simg4ox -g  esi-g4ox/geom/pfrich_min_added_parameters.gdml -m esi-g4ox/run.mac
+./build/src/simg4ox -g  esi-g4ox/geom/pfrich_min_FINAL.gdml -m esi-g4ox/run.mac
