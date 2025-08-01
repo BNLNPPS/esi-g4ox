daf66f490eb271666c012256361dece69dc10537
ggalgoczi <gaborgalgoczi@gmail.com>
2025-08-01 09:52:03 -0400

    Running simple macro

--- scripts/install_root_ratpac.sh
+++ scripts/install_root_ratpac.sh
@@ -31,4 +31,4 @@ git clone https://github.com/rat-pac/ratpac-setup.git
 cd ratpac-setup
 ./setup.sh -j"$(nproc)"
 
-./ratpac/build/bin/rat --help
+./ratpac/build/bin/rat -o output.root -l log.txt ./ratpac/install/share/RAT/macros/examples/electron.mac
