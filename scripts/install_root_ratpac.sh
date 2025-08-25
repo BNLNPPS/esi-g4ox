2e208ab826f6e3ba0b0fb044e513b88b73c4da5b
ggalgoczi <gaborgalgoczi@gmail.com>
2025-08-25 15:54:46 -0400

    Rewrite root install with spack

--- scripts/install_root_ratpac.sh
+++ scripts/install_root_ratpac.sh
@@ -10,19 +10,14 @@ apt-get install -y \
     libgsl-dev libxpm-dev libxft-dev libtbb-dev binutils cmake dpkg-dev \
     g++ gcc libssl-dev git libx11-dev libxext-dev python3 libgif-dev python3 python3-dev
 
+apt-get install -y libgl1-mesa-dev libglu1-mesa-dev
+apt-get install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+
 # ------------------------------------------------------------------
 # 2. Build and install ROOT (latest-stable branch)
 # ------------------------------------------------------------------
-git clone --branch latest-stable --depth 1 https://github.com/root-project/root.git root_src
-mkdir -p root_build root_install
-cd root_build
 
-cmake -DCMAKE_INSTALL_PREFIX="$(pwd)/../root_install" ../root_src
-cmake --build . --target install -j1
-
-# Source ROOT for this shell (optional if you only need it later)
-source "$(pwd)/bin/thisroot.sh"
-cd ..
+spack install root
 
 # ------------------------------------------------------------------
 # 3. Fetch and build RAT-PAC
