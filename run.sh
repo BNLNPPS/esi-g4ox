a73bd40949af0657bf376607e28e7afb81d5e508
ggalgoczi <gaborgalgoczi@gmail.com>
2025-10-01 17:47:46 -0400

    Push slot numbers

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ git checkout PerformanceAnalysis
 cd ..
 QCurandState_SPEC=3000000:0:0 /usr/local/opticks/lib/QCurandStateTest
 export OPTICKS_EVENT_MODE=Minimal
-export OPTICKS_MAX_PHOTON=M60
-export OPTICKS_MAX_SLOT=M60
+export OPTICKS_MAX_PHOTON=M100
+export OPTICKS_MAX_SLOT=M100
 python scripts/cone_opticks_intersection_modify.py
 cmake --build /usr/local/eic-opticks/build --parallel --target install
 cmake -S esi-g4ox -B build
