26702bef0f83009af9d53dc73e208a088cbdad60
ggalgoczi <gaborgalgoczi@gmail.com>
2025-10-02 13:24:49 -0400

    Update run_performance.py

--- scripts/run_performance.py
+++ scripts/run_performance.py
@@ -9,7 +9,7 @@ run_mac_template = """
 /run/verbose 1
 /process/optical/cerenkov/setStackPhotons {flag}
 /run/initialize
-/run/beamOn 160000
+/run/beamOn 320000
 """
 
 def parse_real_time(time_str):
