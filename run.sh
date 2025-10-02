ab1ea805b6e29269f4e178366b96901df6cb3043
ggalgoczi <gaborgalgoczi@gmail.com>
2025-10-02 14:19:15 -0400

    Fix path

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ QCurandState_SPEC=3000000:0:0 /usr/local/opticks/lib/QCurandStateTest
 export OPTICKS_EVENT_MODE=Minimal
 export OPTICKS_MAX_PHOTON=M210
 export OPTICKS_MAX_SLOT=M210
-python scripts/cone_opticks_intersection_modify.py
+python esi-g4ox/scripts/cone_opticks_intersection_modify.py
 cmake --build /usr/local/eic-opticks/build --parallel --target install
 cmake -S esi-g4ox -B build
 cmake --build build
