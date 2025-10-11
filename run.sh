53e496009ab8a9a9bf5f52fc98621e259f02ffc2
ggalgoczi <gaborgalgoczi@gmail.com>
2025-10-11 11:30:42 -0400

    Fix path

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export OPTICKS_EVENT_MODE=Minimal
 export OPTICKS_MAX_PHOTON=M60
 export OPTICKS_MAX_SLOT=M60
 QCurandState_SPEC=60:0:0 /usr/local/opticks/lib/QCurandStateTest
-python scripts/cone_opticks_intersection_modify.py
+python esi-g4ox/scripts/cone_opticks_intersection_modify.py
 cmake --build /usr/local/eic-opticks/build --parallel --target install
 cmake --build build
 ./build/src/simg4ox -g  esi-g4ox/geom/pfrich_min_FINAL.gdml -m esi-g4ox/run.mac
