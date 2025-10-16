2a1d117f28b4ae5640a553ed86e6efc3046848ca
ggalgoczi <gaborgalgoczi@gmail.com>
2025-10-16 12:17:09 -0400

    Uncomment git clone command in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # needs to be copied out to home and called from there
-#git clone https://github.com/BNLNPPS/esi-g4ox
+git clone https://github.com/BNLNPPS/esi-g4ox
 cd esi-g4ox/
 git checkout GenSteps
 poetry install
